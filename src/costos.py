2777be9de59cd2f7d130e546e53df626878512cf
Olimpia Garcia <olimpia.gh24@gmail.com>
2024-11-22 23:44:03 -0600

    feat: costos layout finihed

--- src/costos.py
+++ src/costos.py
@@ -1,11 +1,48 @@
-from dash import html
+from dash import html, dcc
 import pandas as pd
 import plotly.express as px
+from data_manager import load_data, get_unidades 
+
+# Carga los datos desde el archivo CSV
+circuitos = load_data()
+
+# Obtiene las unidades agrupadas y cuenta por tipo de unidad
+unidades = get_unidades(circuitos)
+
+# grafico demo
+fig = px.bar(unidades, x=unidades.index, y=unidades.values, title="Unidades por Tipo")
+
 
 # Página de costos
 costos_page = html.Div(
     [
-        html.H2("Costos", className="page-title"),
-        html.P("Contenido de la página de costos."),
+        html.Div([
+        # Primer fila de boxes
+        html.Div(className="graph", children=[
+            dcc.Graph(id='graph2', figure=fig)
+        ]),
+        html.Div(className="graph", children=[
+            dcc.Graph(id='graph3', figure=fig)  
+        ]),
+    ], style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}),
+
+    html.Div([
+        # Segunda fila de boxes
+        html.Div(className="graph", children=[
+            dcc.Graph(id='graph4', figure=fig)  
+        ]),
+        html.Div(className="graph", children=[
+            dcc.Graph(id='graph5', figure=fig)  
+        ]),
+    ], style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}),
+
+    html.Div([
+        # Primer fila de boxes
+        html.Div(className="graph", children=[
+            dcc.Graph(id='graph6', figure=fig)
+        ]),
+        
+    ], style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}),
+
     ]
 )
