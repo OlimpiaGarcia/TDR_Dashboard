31dfc88333d402dc9b2dc2afe8dd9f2ec59d17c5
Olimpia Garcia <olimpia.gh24@gmail.com>
2024-11-23 16:26:57 -0600

    Fix: adjust layout for metricas, looking like figma

--- src/metricas.py
+++ src/metricas.py
@@ -59,6 +59,28 @@ metricas_page = html.Div(
                         )
                     ]
                 ),
+                html.Div(
+                    [
+                        html.H1("Mean Time to Repair (MTTR)"),
+                        dash_table.DataTable(
+                            columns=[
+                                {"name": "Medida", "id": "Medida"},
+                                {"name": "Valor", "id": "Valor"}
+                            ],
+                            data=describe.to_dict('records'),
+                            style_cell={'textAlign': 'left'},
+                            style_header={
+                                'backgroundColor': 'white',
+                                'fontWeight': 'bold'
+                            },
+                            style_data_conditional=[
+                                {'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(248, 248, 248)'},
+                                {'if': {'row_index': 'even'}, 'backgroundColor': 'white'}
+                            ]
+                        )
+                    ]
+                ),
+
             ],
             style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}
         ),
@@ -112,32 +134,5 @@ metricas_page = html.Div(
             style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}
         ),
 
-        # Tercera fila de boxes
-        html.Div(
-            [
-                html.Div(
-                    [
-                        html.H1("Mean Time to Repair (MTTR)"),
-                        dash_table.DataTable(
-                            columns=[
-                                {"name": "Medida", "id": "Medida"},
-                                {"name": "Valor", "id": "Valor"}
-                            ],
-                            data=describe.to_dict('records'),
-                            style_cell={'textAlign': 'left'},
-                            style_header={
-                                'backgroundColor': 'white',
-                                'fontWeight': 'bold'
-                            },
-                            style_data_conditional=[
-                                {'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(248, 248, 248)'},
-                                {'if': {'row_index': 'even'}, 'backgroundColor': 'white'}
-                            ]
-                        )
-                    ]
-                ),
-            ],
-            style={'display': 'flex', 'justify-content': 'center', 'margin': '10px 0'}
-        ),
     ]
 )
