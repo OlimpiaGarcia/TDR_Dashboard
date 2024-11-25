a99ccff43d6c5899446b97e61a433d08125e7ced
Olimpia Garcia <olimpia.gh24@gmail.com>
2024-11-24 21:14:18 -0600

    Feat: add data and correct titles to page, style not finished yet

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,15 +1,145 @@
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 # Cargamos los datos
 def load_data():
     circuitos = pd.read_csv("./data/circuitos.csv")
     return circuitos
 
-''' Pagina de estadistica'''
 
 # Cargamos los datos de unidades
 def get_unidades(circuitos):
     unidades = circuitos.groupby(['TipoUnidad'])['Unidad'].nunique()
     return unidades
 
+# creamos circuitos
+circuitos = load_data()
+
+# cragamos el csv de TOT
+TOT = pd.read_excel('./data/TOT.xlsx')
+
+# Crear etiquetas para cada trimestre
+etiquetas_trimestres = [
+    'T1_2022', 'T2_2022', 'T3_2022', 'T4_2022',
+    'T1_2023', 'T2_2023', 'T3_2023', 'T4_2023',
+    'T1_2024', 'T2_2024', 'T3_2024']
+
+
+""" Crear Horas fuera de operación (Downtime) por unidad, Total de incidentes, Tiempo medio entre fallos (MTBF) y Tiempo medio para reparar (MTTR)"""
+# copia de circuitos
+lagdias_per_unit = circuitos.copy()
+
+# Asegúrate de que la columna 'Trimestre' esté en el orden definido
+lagdias_per_unit['Trimestre'] = pd.Categorical(lagdias_per_unit['Trimestre'], categories=etiquetas_trimestres, ordered=True)
+
+# Agrupar por unidad y trimestre, y calcular el promedio de 'lagdias', agregar 0
+mean_lagdias_by_unit_trimester = lagdias_per_unit.groupby(['Unidad', 'Trimestre'])['lagdias'].mean().round(2).reset_index(name='Downtime')
+
+# Ordenar por 'T_UNITNUMBER' y 'Trimestre' siguiendo el orden de trimestres
+mean_lagdias_sorted = mean_lagdias_by_unit_trimester.sort_values(by=['Unidad', 'Trimestre'])
+
+# Agregar 0 a las columnas vacias
+mean_lagdias_sorted['Downtime'] = mean_lagdias_sorted['Downtime'].fillna(0)
+
+# incidentes
+incidentes = circuitos.copy()
+
+# Asegúrate de que la columna 'Trimestre' esté en el orden definido
+incidentes['Trimestre'] = pd.Categorical(incidentes['Trimestre'], categories=etiquetas_trimestres, ordered=True)
+
+#Agrupar por unidad y trimwstre y calcular cuantos orderid unicos hay
+incidentes_by_unit_trimester = incidentes.groupby(['Unidad', 'Trimestre'])['Orderid'].nunique().reset_index(name='No.incidentes')
+
+# Ordenar por 'T_UNITNUMBER' y 'Trimestre' siguiendo el orden de trimestres
+incidentes_sorted = incidentes_by_unit_trimester.sort_values(by=['Unidad', 'Trimestre'])
+
+# Agregar 0 a las columnas vacias
+incidentes_sorted['No.incidentes'] = incidentes_sorted['No.incidentes'].fillna(0)
+
+# juntar DF incidentes_sorted y  mean_lagdias_sorted
+fix_metrics = incidentes_sorted.merge(mean_lagdias_sorted, on=['Unidad', 'Trimestre'])
+
+# convertir columna unidad en numero entero
+fix_metrics['Unidad'] = fix_metrics['Unidad'].astype(int)
+
+"""Hacvemos calculos con el archivo TOT"""	
+
+# renombrar columna 'T_UNITNUMBER' a 'Unidad'
+TOT = TOT.rename(columns={'T_UNITNUMBER': 'Unidad', 'days': 'TOT'})
+
+# pasar trimestre a category
+TOT['Trimestre'] = pd.Categorical(TOT['Trimestre'], categories=etiquetas_trimestres, ordered=True)
+
+# hacer unidad index
+TOT = TOT.set_index('Unidad')
+
+# drop Unnamed: 0
+TOT = TOT.drop(columns=['Unnamed: 0'])
+
+# juntar DF de MTBF y TOT
+fix_metrics = fix_metrics.merge(TOT, on=['Unidad', 'Trimestre'])
+
+#Eliminar columnas vacias
+fix_metrics = fix_metrics.dropna()
+
+# # Asegúrate de que la columna 'Trimestre' esté en el orden definido
+fix_metrics['Trimestre'] = pd.Categorical(fix_metrics['Trimestre'], categories=etiquetas_trimestres, ordered=True)
+
+# calcular MTBF
+fix_metrics['MTBF'] = (fix_metrics['TOT'] / fix_metrics['No.incidentes']).round(2)
+
+# si mtbf es inf rellenar con TOT
+fix_metrics['MTBF'] = fix_metrics['MTBF'].replace(np.inf, np.nan)
+fix_metrics['MTBF'] = fix_metrics['MTBF'].fillna(fix_metrics['TOT'])
+
+# calcular mttr
+fix_metrics['MTTR'] = (fix_metrics['Downtime'] / fix_metrics['No.incidentes']).round(2)
+
+# si mttr es inf rellenat con 0
+fix_metrics['MTTR'] = fix_metrics['MTTR'].replace(np.inf, np.nan)
+fix_metrics['MTTR'] = fix_metrics['MTTR'].fillna(0)
+
+# Ordenar por trimestre
+fix_metrics_sorted = fix_metrics.sort_values(by=['Unidad', 'Trimestre'])
+
+"""hacemos archivo de regresion"""
+regresion = circuitos.copy()
+regresion['Trimestre'] = pd.Categorical(regresion['Trimestre'], categories=etiquetas_trimestres, ordered=True)
+regresion['Unidad'] = regresion['Unidad'].astype(int)
+regresion = regresion.merge(fix_metrics_sorted, on=['Unidad', 'Trimestre'])
+regresion_limpio = regresion[regresion['opened'] != regresion['closed']]
+clean_data_subtotal = regresion_limpio[['MTBF', 'MTTR', 'Downtime', 'SUBTOTAL']].dropna() 
+
+
+""""Cargamos los df necesarios para las estadisticas:"""
+
+costos_por_tipo_unidad = circuitos.groupby('TipoUnidad')['SUBTOTAL'].sum()
+costos_por_unidad = circuitos.groupby('Unidad')['SUBTOTAL'].sum().sort_values(ascending=False)
+costos_por_año_tipo_unidad = circuitos.groupby(['aniocomplete', 'TipoUnidad'])['SUBTOTAL'].sum()
+costos_por_año_unidad = circuitos.groupby(['aniounidad', 'Unidad'])['SUBTOTAL'].sum()
+costos_por_añodeunidad = circuitos.groupby(['aniounidad'])['SUBTOTAL'].sum()
+costo_por_jobcode = circuitos.groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+costo_por_TipoM = circuitos.groupby('TipoM')['SUBTOTAL'].sum().sort_values(ascending=False)
+costo_por_jobcode = circuitos.groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+costo_por_jobcode_unplanned = circuitos[circuitos['TipoM'].isin(['Correctivo', 'Otro'])].groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+costo_por_jobcode_planned=circuitos[circuitos['TipoM'] == 'Preventivo'].groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+df_TipoAB=circuitos[circuitos['jobcode'].str.contains('preventivo A|preventivo B', na=False)]
+df_TipoAB=df_TipoAB[df_TipoAB['jobcode'] != '000074 - Derivado de preventivo Arrastre']
+cantidad_por_TipoAB=df_TipoAB.groupby('jobcode')['Orderid'].count().sort_values(ascending=False)
+costo_por_TipoAB=df_TipoAB.groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+df_TipoM123=circuitos[circuitos['jobcode'].str.contains('M1|M2|M3', na=False)]
+cantidad_por_TipoM123=df_TipoM123.groupby('jobcode')['Orderid'].count()
+costo_por_TipoM123=df_TipoM123.groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+df_TipoSH12=circuitos[circuitos['jobcode'].str.contains('SH 1|SH 2', na=False)]
+cantidad_por_TipoSH12=df_TipoSH12.groupby('jobcode')['Orderid'].count()
+costo_por_TipoSH12=df_TipoSH12.groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+df_Tipollantas=circuitos[circuitos['jobcode'].str.contains('llanta|Llantas|Llanta', na=False)]
+cantidad_por_Tipollantas=df_Tipollantas.groupby('jobcode')['Orderid'].count()
+costo_por_Tipollantas=df_Tipollantas.groupby('jobcode')['SUBTOTAL'].sum().sort_values(ascending=False)
+regresion_limpio_tractor = regresion_limpio[regresion_limpio['TipoUnidad'] == 'TRACTOR']
+regresion_limpio_tractor = regresion_limpio_tractor.dropna(subset=['Kilometraje'])
+regresion_limpio_trailer = regresion_limpio[regresion_limpio['TipoUnidad'] == 'TRAILER']
+correct_data = regresion_limpio.copy()
+selected_features = correct_data[['MTBF', 'MTTR', 'Downtime', 'lagdias', 'No.incidentes']].copy()
+selected_features = selected_features.dropna()
--- src/metricas.py
+++ src/metricas.py
@@ -1,14 +1,48 @@
 from dash import html
 from dash import dash_table
+import pandas as pd
 import plotly.express as px
-from data_manager import load_data, get_unidades 
+from data_manager import circuitos, fix_metrics
 
-# Carga los datos desde el archivo CSV
-circuitos = load_data()
+# Describe de Mttr:
+mttr_desc = fix_metrics['MTTR'].describe().round(2)
+mttr_desc = pd.DataFrame(mttr_desc)
+mttr = [
+    {"Estadística": index, "Valor": row['MTTR']}
+    for index, row in mttr_desc.iterrows()
+]
 
-# hacer un describe
-describe = circuitos.describe()
+# Describe de Mtbf:
+mtbf_desc = fix_metrics['MTBF'].describe().round(2)
+mtbf_desc = pd.DataFrame(mtbf_desc)
+mtbf = [
+    {"Estadística": index, "Valor": row['MTBF']}
+    for index, row in mtbf_desc.iterrows()
+]
 
+# Describe de Downtime:
+downtime_desc = fix_metrics['Downtime'].describe().round(2)
+downtime_desc = pd.DataFrame(downtime_desc)
+downtime = [
+    {"Estadística": index, "Valor": row['Downtime']}
+    for index, row in downtime_desc.iterrows()
+]
+
+# Describe de failure rate:
+failure_rate_desc = circuitos['lagdias'].describe().round(2)
+failure_rate_desc = pd.DataFrame(failure_rate_desc)
+failure_rate = [
+    {"Estadística": index, "Valor": row['lagdias']}
+    for index, row in failure_rate_desc.iterrows()
+]
+
+# Describe de Matenimineto:
+costos_mtto_desc = circuitos[['Unidad', 'SUBTOTAL']].describe()
+costos_mtto_desc = pd.DataFrame(costos_mtto_desc['SUBTOTAL'].round(2))
+mtto = [
+    {"Estadística": index, "Valor": row['SUBTOTAL']}
+    for index, row in costos_mtto_desc.iterrows()
+]
 
 
 # Página de métricas
@@ -22,34 +56,13 @@ metricas_page = html.Div(
                         html.H1("Mean Time to Repair (MTTR)"),
                         dash_table.DataTable(
                             columns=[
-                                {"name": "Medida", "id": "Medida"},
+                               {"name": "Estadística", "id": "Estadística"},
                                 {"name": "Valor", "id": "Valor"}
                             ],
-                            data=describe.to_dict('records'),
-                            style_cell={'textAlign': 'left'},
+                            data=mttr,
+                            style_cell={'textAlign': 'center'},
                             style_header={
-                                'backgroundColor': 'white',
-                                'fontWeight': 'bold'
-                            },
-                            style_data_conditional=[
-                                {'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(248, 248, 248)'},
-                                {'if': {'row_index': 'even'}, 'backgroundColor': 'white'}
-                            ]
-                        )
-                    ],
-                ),
-                html.Div(
-                    [
-                        html.H1("Mean Time to Repair (MTTR)"),
-                        dash_table.DataTable(
-                            columns=[
-                                {"name": "Medida", "id": "Medida"},
-                                {"name": "Valor", "id": "Valor"}
-                            ],
-                            data=describe.to_dict('records'),
-                            style_cell={'textAlign': 'left'},
-                            style_header={
-                                'backgroundColor': 'white',
+                                'backgroundColor': '#b6c7e5ff',
                                 'fontWeight': 'bold'
                             },
                             style_data_conditional=[
@@ -61,16 +74,37 @@ metricas_page = html.Div(
                 ),
                 html.Div(
                     [
-                        html.H1("Mean Time to Repair (MTTR)"),
+                        html.H1("Mean Time Between Failures (MTBF)"),
                         dash_table.DataTable(
                             columns=[
-                                {"name": "Medida", "id": "Medida"},
+                               {"name": "Estadística", "id": "Estadística"},
                                 {"name": "Valor", "id": "Valor"}
                             ],
-                            data=describe.to_dict('records'),
-                            style_cell={'textAlign': 'left'},
+                            data=mttr,
+                            style_cell={'textAlign': 'center'},
                             style_header={
-                                'backgroundColor': 'white',
+                                'backgroundColor': '#fbe1b0ff',
+                                'fontWeight': 'bold'
+                            },
+                            style_data_conditional=[
+                                {'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(248, 248, 248)'},
+                                {'if': {'row_index': 'even'}, 'backgroundColor': 'white'}
+                            ]
+                        )
+                    ]
+                ),
+                html.Div(
+                    [
+                        html.H1("Downtime"),
+                        dash_table.DataTable(
+                            columns=[
+                               {"name": "Estadística", "id": "Estadística"},
+                                {"name": "Valor", "id": "Valor"}
+                            ],
+                            data=downtime,
+                            style_cell={'textAlign': 'center'},
+                            style_header={
+                                'backgroundColor': '#b6c7e5ff',
                                 'fontWeight': 'bold'
                             },
                             style_data_conditional=[
@@ -82,7 +116,7 @@ metricas_page = html.Div(
                 ),
 
             ],
-            style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}
+            style={'display': 'flex', 'justify-content': 'space-around', 'margin': '1px 0'}
         ),
 
         # Segunda fila de boxes
@@ -90,16 +124,16 @@ metricas_page = html.Div(
             [
                 html.Div(
                     [
-                        html.H1("Mean Time to Repair (MTTR)"),
+                        html.H1("Failure Rate"),
                         dash_table.DataTable(
                             columns=[
-                                {"name": "Medida", "id": "Medida"},
+                                {"name": "Estadística", "id": "Estadística"},
                                 {"name": "Valor", "id": "Valor"}
                             ],
-                            data=describe.to_dict('records'),
-                            style_cell={'textAlign': 'left'},
+                            data=failure_rate,
+                            style_cell={'textAlign': 'center'},
                             style_header={
-                                'backgroundColor': 'white',
+                                'backgroundColor': '#f68d2bff',
                                 'fontWeight': 'bold'
                             },
                             style_data_conditional=[
@@ -111,16 +145,16 @@ metricas_page = html.Div(
                 ),
                 html.Div(
                     [
-                        html.H1("Mean Time to Repair (MTTR)"),
+                        html.H1("Costos de Mantenimiento"),
                         dash_table.DataTable(
                             columns=[
-                                {"name": "Medida", "id": "Medida"},
+                               {"name": "Estadística", "id": "Estadística"},
                                 {"name": "Valor", "id": "Valor"}
                             ],
-                            data=describe.to_dict('records'),
-                            style_cell={'textAlign': 'left'},
+                            data=mtto,
+                            style_cell={'textAlign': 'center'},
                             style_header={
-                                'backgroundColor': 'white',
+                                'backgroundColor': '#b6c7e5ff',
                                 'fontWeight': 'bold'
                             },
                             style_data_conditional=[
@@ -131,7 +165,7 @@ metricas_page = html.Div(
                     ]
                 ),
             ],
-            style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}
+            style={'display': 'flex', 'justify-content': 'space-around', 'margin': '1px 0'}
         ),
 
     ]
