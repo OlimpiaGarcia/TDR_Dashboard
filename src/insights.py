7d35803308f778d18ace32ad497bf2439a16421f
Olimpia Garcia <olimpia.gh24@gmail.com>
2024-11-26 18:38:07 -0600

    Feat: add new text to insights

--- src/insights.py
+++ src/insights.py
@@ -1,6 +1,6 @@
 from dash import html, dcc
 import plotly.express as px
-from data_manager import model_multiple, load_data, get_unidades 
+from data_manager import model_multiple, load_data, get_unidades, clean_data_subtotal  
 
 # Carga los datos desde el archivo CSV
 circuitos = load_data()
@@ -9,14 +9,43 @@ circuitos = load_data()
 unidades = get_unidades(circuitos)
 
 '''Data de la pagina'''
+
+# Data 1 Regrssion multiple
 results_summary = model_multiple.summary().as_text()
 markdown_text = f"""
 # 
 ## {results_summary}
 # """
 
+# Data 2 Grficas
+clean_data_subtotal = clean_data_subtotal
+
+
+# Data 3 Conclusiones
+
 
 '''Graficas de la pagina'''
+
+#grafico 1:
+fig1 = px.scatter(
+    clean_data_subtotal,
+    x='MTTR',
+    y='SUBTOTAL',
+    title='MTTR vs Costos',
+    labels={'MTTR': 'MTTR', 'SUBTOTAL': 'Costos'}
+)
+
+# grafica 2
+fig2 = px.scatter(
+    clean_data_subtotal,
+    x='Downtime',
+    y='SUBTOTAL',
+    title='Downtime vs Costor',
+    labels={'Downtime': 'Downtime', 'SUBTOTAL': 'Costos'}
+)
+
+
+
 # grafico demo
 fig = px.bar(unidades, x=unidades.index, y=unidades.values, title="Unidades por Tipo")
 
@@ -33,10 +62,10 @@ insights_page = html.Div(
 
         html.Div([
         html.Div(className="graph", children=[
-            dcc.Graph(id='graph2', figure=fig)
+            dcc.Graph(id='graph1', figure=fig1)
         ]),
         html.Div(className="graph", children=[
-            dcc.Graph(id='graph3', figure=fig)  
+            dcc.Graph(id='graph2', figure=fig2)  
         ]),
         ], style={'display': 'flex', 'justify-content': 'space-around', 'margin': '10px 0'}),
 
@@ -45,8 +74,9 @@ insights_page = html.Div(
             [
                 html.Div(className="text", children=[
                     html.H1("Insights"),
-                    html.P("En esta página se presentan los resultados de los análisis realizados."),
-                    html.P("Se presentan los resultados de los análisis realizados."),                
+                    html.P("El coeficiente es -23.2649 y es estadísticamente significativo (p = 0.003). Esto sugiere que un mayor tiempo para reparar (MTTR) tiene un efecto negativo sobre los costos de mantenimiento, es decir, a medida que aumenta el MTTR, los costos disminuyen, lo cual puede ser contraintuitivo."),
+                    html.P("El coeficiente es 13.2195 y es estadísticamente significativo (p = 0.005). Esto indica que a mayor Downtime, los costos de mantenimiento tienden a aumentar."),
+               
                 ]), 
             ]
         ),
